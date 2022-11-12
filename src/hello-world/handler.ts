f67c9545448be3aa2512da0202828f094520eab5
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-12 01:15:24 -0600

    refactor: put stage variable

--- src/hello-world/handler.ts
+++ src/hello-world/handler.ts
@@ -1,8 +1,9 @@
 export async function handler(event: string, context: string){
     console.log("EVENT:",event);
     console.log("CONTEXT:",context);
+
     return {
-        body: `Hello from a Lambda Function ${process.env.stage}`,
+        body: `Hello from a Lambda Function ${process.env.stageName}`,
         statusCode: 200
     };
 }
