82999fceef3a6db87efb179bf3a4c3c5b57b3889
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-12 01:07:36 -0600

    refactor: add stage name

--- src/hello-world/handler.ts
+++ src/hello-world/handler.ts
@@ -1,4 +1,6 @@
 export async function handler(event: string, context: string){
+    console.log("EVENT:",event);
+    console.log("CONTEXT:",context);
     return {
         body: `Hello from a Lambda Function ${process.env.stage}`,
         statusCode: 200
